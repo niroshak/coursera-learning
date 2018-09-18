b3478ffe1c38e4e0d0708ee8afb5222b0667190f
niroshak <kay.nirosha@gmail.com>
2018-09-18 22:00:45 +0530

    login

---
+++ site/1.js
@@ -0,0 +1,27 @@
+
+ var persons=[
+{
+	name: "nirosha",
+	password: "nirosha123",
+	email id: "kay.nirosha@gmail.com"
+},
+{
+	name: "pinky",
+	password: "pinky@1",
+	email id: "pinky@gmail.com"
+}]
+
+var register={}
+function Registration(){
+	register.name=document.getElementById('name').value;
+	register.password=document.getElementById('password').value;
+	register.emailid=document.getElementById('emailid').value;
+	return register;
+}
+persons.push(register);
+
+
+
+
+
+	
